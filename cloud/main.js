90dda2d28aa5014b6211508f3a80ac89842f32d4
Justin Steffen <JustinDSN@gmail.com>
2013-11-02 13:52:12 -0700

    Adding code to send specific users specfic notifications when the recieve a message.

--- cloud/main.js
+++ cloud/main.js
@@ -1,29 +1,18 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.afterSave("Message", function(request) {
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('user', request.object.get('receiver'));
+        
+  Parse.Push.send({
+      where: pushQuery,
+      data: {
+        alert: "New message from " + request.user.get('name')
+      }
+  }, {
+    success: function() {
+      // Push was successful
+    },
+    error: function(error) {
+      throw "Got an error " + error.code + " : " + error.message;
+    }
+  });
 });
-
-
-// Parse.Cloud.afterSave("Message", function(request) {
-//   // Our "Comment" class has a "text" key with the body of the comment itself
-//   var commentText = request.object.get('text');
- 
-//   var pushQuery = new Parse.Query(Parse.Installation);
-//   pushQuery.equalTo('deviceType', 'ios');
-    
-//   Parse.Push.send({
-//     where: pushQuery, // Set our Installation query
-//     data: {
-//       alert: "New comment: " + commentText
-//     }
-//   }, {
-//     success: function() {
-//       // Push was successful
-//     },
-//     error: function(error) {
-//       throw "Got an error " + error.code + " : " + error.message;
-//     }
-//   });
-// });
